ba88e4233f8bfb026670f291a1d5864ab898bb2b
nookesh <nookeshkarri7@gmail.com>
2021-09-28 16:35:53 +0530

    created detailed view class added urls

--- todolist/todoapp/urls.py
+++ todolist/todoapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path("",views.TaskList.as_view(),name='TaskList')
-
+    path("",views.TaskList.as_view()),
+    path("task/<int:pk>/",views.TaskDetail.as_view())
 ]
--- todolist/todoapp/views.py
+++ todolist/todoapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from todoapp.models import Task
 class TaskList(ListView):
     model=Task
+
+class TaskDetail(DetailView):
+    model=Task
+    context_object_name='task'
